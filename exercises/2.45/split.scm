b232c06b6016c5fdd03954ef73d67b2a6f147914
bitterlox <bitterlox@pm.me>
2024-02-28 11:01:54 +0100

    fix thing;

--- exercises/2.45/split.scm
+++ exercises/2.45/split.scm
@@ -1,3 +1,7 @@
 (define (split fn1 fn2)
-  (lambda (painter)
-    (fn1 painter (fn2 painter))))
+  (lambda (painter n)
+    (cond
+      ((= n 0) painter)
+      (else (let
+              ((smaller ((split fn1 fn2) painter (- n 1)))
+               (fn1 painter (fn2 smaller smaller))))))))
