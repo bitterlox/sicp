d00207c57d34bc786b485f132927fa9cebde9c48
bitterlox <bitterlox@pm.me>
2024-02-29 09:41:54 +0100

    ex 2.50;

---
+++ exercises/2.50/exercise.scm
@@ -0,0 +1,29 @@
+(define (flip-horiz painter)
+  (transform-painter
+    painter
+    (make-vect 1.0 0.0) ; new origin
+    (make-vect 0.0 0.0) ; new end of edge1
+    (make-vect 1.0 1.0))) ; new end of edge2
+
+
+(define (rotate90 painter)
+  (transform-painter
+    painter
+    (make-vect 1.0 0.0)
+    (make-vect 1.0 1.0)
+    (make-vect 0.0 0.0)))
+
+
+(define (rotate180 painter)
+  (transform-painter
+    painter
+    (make-vect 1.0 1.0)
+    (make-vect 0.0 1.0)
+    (make-vect 1.0 0.0)))
+
+(define (rotate270 painter)
+  (transform-painter
+    painter
+    (make-vect 0.0 1.0)
+    (make-vect 0.0 0.0)
+    (make-vect 1.0 1.0)))
