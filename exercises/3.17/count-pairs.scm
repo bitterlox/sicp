bf18365e904263644462c90124b401eec8d26d30
bitterlox <bitterlox@pm.me>
2024-04-16 10:32:53 +0200

    did ex 3.17;

--- exercises/3.17/count-pairs.scm
+++ exercises/3.17/count-pairs.scm
@@ -1,24 +1,24 @@
-(define (count-pairs-old x) 
-   (if (not (pair? x)) 
-       0 
-       (+ (count-pairs-old (car x)) 
-          (count-pairs-old (cdr x)) 
-          1)))
-
 (define (count-pairs x) 
-  (define (is-in-seen-pairs seen-pairs pair)
-    (if
-      (= (length seen-pairs) 0)
-      #f
-      (find
-            (lambda (p) (equal? p pair))
+  (let
+    ((seen-pairs '()))
+    (begin
+      (define (is-in-seen-pairs pair)
+        (if
+          (= (length seen-pairs) 0)
+          #f
+          (find
+            (lambda (p) (eq? p pair))
             seen-pairs)))
-  (define (loop seen-pairs pairs)
-    (if (or
-          (not (pair? pairs))
-          (is-in-seen-pairs seen-pairs pairs))
-      0 
-      (+ (loop (cons pairs seen-pairs) (car pairs)) 
-         (loop (cons pairs seen-pairs) (cdr pairs)) 
-         1)))
-  (loop '() x))
+      (define (loop pairs)
+        (cond
+          ((or
+             (not (pair? pairs))
+             (is-in-seen-pairs pairs))
+           0)
+          (else
+            (set! seen-pairs (cons pairs seen-pairs))
+            (+
+              (loop (car pairs))
+              (loop (cdr pairs))
+              1)))))
+    (loop x)))
