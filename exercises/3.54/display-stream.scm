37cfdf48831bd4ce1e6e70a9db33e36fd4bda867
bitterlox <bitterlox@pm.me>
2024-05-06 16:57:27 +0200

    did exercises;

--- exercises/3.54/display-stream.scm
+++ exercises/3.54/display-stream.scm
@@ -1,2 +1,3 @@
+(define (display-stream s) (stream-for-each display-line s))
 
 (define (display-line x) (newline) (display x))
---
+++ exercises/3.66/exercise.scm
@@ -0,0 +1,3 @@
+(load "/home/angel/sicp/exercises/3.54/mul-streams.scm")
+(load "/home/angel/sicp/exercises/3.54/display-stream.scm")
+(load "/home/angel/sicp/exercises/3.66/pairs.scm")
